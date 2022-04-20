9215394eec83f012b0e5ee64eda23d57a2e7b3a6
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-04-20 23:58:43 +0200

    Use `address(0)` instead of NULLADDR constant

--- src/ProofOfLove.sol
+++ src/ProofOfLove.sol
@@ -6,11 +6,10 @@ contract ProofOfLove {
 
     mapping (address => address) public proposals;
     mapping (address => address) public lovers;
-    address private constant NULLADDR = address(0x000000000000000000000000000000000);
 
     function propose(address _to) public returns (bool success) {
-        require(lovers[msg.sender] == NULLADDR, "you're already in love...");
-        require(lovers[_to] == NULLADDR, "looks like they already have someone else : (");
+        require(lovers[msg.sender] == address(0), "you're already in love...");
+        require(lovers[_to] == address(0), "looks like they already have someone else : (");
         // theyve already proposed, accept!! <3
         if (proposals [_to] == msg.sender) {
             lovers[msg.sender] = _to;
@@ -23,13 +22,13 @@ contract ProofOfLove {
     }
 
     function breakUp() public returns (bool success) {
-        require(lovers [msg.sender] != NULLADDR, "NO MAIDENS?");
+        require(lovers [msg.sender] != address(0), "NO MAIDENS?");
 
         if (lovers[lovers[msg.sender]] == msg.sender) {
-            lovers[lovers[msg.sender]] = NULLADDR;
+            lovers[lovers[msg.sender]] = address(0);
         }
 
-        lovers[msg.sender] = NULLADDR;
+        lovers[msg.sender] = address(0);
 
         return true;
     }
